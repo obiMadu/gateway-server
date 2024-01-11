07a60a757265729f7d65b8f7abf811f32449385c
Obi.M <mail@obimadu.pro>
2024-01-11 18:04:04 +0100

    create ansible test run bash script

---
+++ config/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+filename="../inventory"  # Replace with the actual filename
+
+# Using 'cat' and checking for empty output:
+if [ -z "$(cat "$filename")" ]; 
+then
+  echo "out=$(echo "Nothing in the Inventory.")" >> "$GITHUB_OUTPUT"; 
+else
+    ansible all -m ping --check;
+    echo "out=$(cat out.log)" >> "$GITHUB_OUTPUT";
+fi
