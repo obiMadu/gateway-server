7c56573c107b278065e9fc678c05a2c82ebc273a
Obi.M <mail@obimadu.pro>
2024-01-12 22:48:52 +0100

    modify test.sh

--- config/test.sh
+++ config/test.sh
@@ -4,9 +4,9 @@ filename="../inventory"  # Replace with the actual filename
 
 cat $filename
 
-# Using 'cat' and checking for empty output:
+# Using 'wc' and checking for empty output:
 if [ "$(wc -c < "$filename")" -eq 0 ]; then
-  echo "out=$(echo "Nothing in the Inventory.")" >> "$GITHUB_OUTPUT"; 
+  echo "out=$(echo 'Nothing in the Inventory.')" >> "$GITHUB_OUTPUT"; 
 else
     ansible all -m ping --check;
     echo "out=$(cat out.log)" >> "$GITHUB_OUTPUT";
