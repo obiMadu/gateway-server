a85f1f59243c821876a9b89f77f87858afb5e8f4
Obi.M <mail@obimadu.pro>
2024-01-12 22:51:28 +0100

    (debug) remove else clause

--- config/test.sh
+++ config/test.sh
@@ -7,7 +7,4 @@ cat $filename
 # Using 'wc' and checking for empty output:
 if [ "$(wc -c < "$filename")" -eq 0 ]; then
   echo "out=$(echo 'Nothing in the Inventory.')" >> "$GITHUB_OUTPUT"; 
-else
-    ansible all -m ping --check;
-    echo "out=$(cat out.log)" >> "$GITHUB_OUTPUT";
 fi
