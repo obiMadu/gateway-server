6415872a9fc6f5880ba83c580c2c6cffa2eba6b8
Obi.M <mail@obimadu.pro>
2024-01-12 23:53:20 +0100

    use python instead to genInventory

---
+++ config/genInventory.py
@@ -0,0 +1,16 @@
+import os
+import subprocess
+
+# Change directory to '../deploy'
+os.chdir("../deploy")
+
+# Execute Terraform command and capture output
+try:
+    gate_ip = subprocess.check_output(["terraform", "output", "--raw", "gate_ip"], text=True).strip()
+except subprocess.CalledProcessError as error:
+    print(f"Error executing Terraform command: {error}")
+    exit(1)  # Exit with error code
+
+# Write gate_ip to the inventory file
+with open("../inventory", "w") as file:
+    file.write(gate_ip)
