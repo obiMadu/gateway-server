890dce3b15a7a0a0f60898fe15847ced229c52e8
Obi.M <mail@obimadu.pro>
2024-01-13 18:22:16 +0100

    set proper ansible dry-run command

--- config/test.sh
+++ config/test.sh
@@ -5,7 +5,7 @@ filename="../inventory"  # Replace with the actual filename
 regex_pattern="^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$"  # Example regex for IPv4 address
 
 if [[ "$(cat "$filename")" =~ $regex_pattern ]]; then
-    ansible all -m ping --check;
+    ansible-playbook deploy.yml --check;
     echo "out=$(cat out.log)" >> $GITHUB_OUTPUT;
 else
     echo "out=$(echo 'Nothing in the Inventory.')" >> $GITHUB_OUTPUT; 
