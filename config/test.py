594d15491ac4c43bfef4488ab96593b5109aa6e7
Obi.M <mail@obimadu.pro>
2024-01-13 12:44:32 +0100

    modify test.py

--- config/test.py
+++ config/test.py
@@ -1,19 +1,20 @@
 import os
 import subprocess
+import re
 
 filename = "../inventory"  # Replace with the actual filename
 
 # Check if the file is empty after removing whitespace and control characters:
 with open(filename, "r") as file:
-    content = file.read()
-    print("Inventory file content: " + "'" + content + "'")
-    if ( content == " " ):  # Check if content is empty
-        output = "Nothing in the Inventory."
-    else:
+    # content = file.read()
+    # print("Inventory file content: " + "'" + content + "'")
+    if ( re.match("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", file.readline().strip()) ):  # Check if content matches an IPAddress
         # Run Ansible command and capture output
         subprocess.run(["ansible", "all", "-m", "ping", "--check"], check=True)
         with open("out.log", "r") as logfile:
             output = logfile.read()
+    else:
+        output = "Nothing in the Inventory."        
 
 # Append output to GITHUB_OUTPUT environment variable
 # os.environ["GITHUB_OUTPUT"] += f"out=({output})"
